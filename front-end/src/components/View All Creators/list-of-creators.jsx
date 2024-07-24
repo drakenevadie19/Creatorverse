f907d4701e9e221a4ca7e6bdfbd53e36a4d81bd3
drakenevadie19 <drakennievadje19@gmail.com>
2024-07-24 16:29:56 -0500

    New Change: Adding Searching box with CSS

--- front-end/src/components/View All Creators/list-of-creators.jsx
+++ front-end/src/components/View All Creators/list-of-creators.jsx
@@ -30,9 +30,12 @@ const ShowCreators = () => {
                     searching === false 
                         ?
                         <div className="searchBar" onClick={() => setSearching(true)}>
-                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
-                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                            </svg>
+                            <span className="search-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                                </svg> 
+                                Search
+                            </span>
                         </div>
                         : 
                         <div className="searchBar">
@@ -42,10 +45,11 @@ const ShowCreators = () => {
                                 </svg>
                             </span> */}
                             <input type="search" className="searchInput" name="search" placeholder="Search" aria-label="Search" style={{marginBottom: "0"}} />
-                            <span className="input-group-text" onClick={() => setSearching(false)} style={{padding: "20px"}}>
+                            <span className="input-group-text cancel-search" onClick={() => setSearching(false)} style={{padding: "15px"}}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-x-lg" viewBox="0 0 16 16">
                                     <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
                                 </svg>
+                                Cancel
                             </span>
                         </div>
                 }
